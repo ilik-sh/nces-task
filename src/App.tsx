50ddbab08701da476db1e70035acad463241190b
Ilya <i.shyshparonak@gmail.com>
2024-04-04 17:03:57 +0300

    feat: change app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "app.routes";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "themes/theme";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorPage from "components/error.page";
+import { SnackbarProvider } from "notistack";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ErrorBoundary fallback={<ErrorPage />}>
+        <SnackbarProvider>
+          <ThemeProvider theme={theme}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <BrowserRouter>
+                <AppRoutes />
+              </BrowserRouter>
+            </LocalizationProvider>
+          </ThemeProvider>
+        </SnackbarProvider>
+      </ErrorBoundary>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
